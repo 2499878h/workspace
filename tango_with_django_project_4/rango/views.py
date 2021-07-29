3a38d8e1fe114deb02049a8a96272dc1a05ec4e0
2499878H <2499878h@student.gla.ac.uk>
2021-07-29 07:23:49 +0100

    new commit

--- tango_with_django_project_4/rango/views.py
+++ tango_with_django_project_4/rango/views.py
@@ -5,27 +5,15 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    #Method 1
-    html = "Please check the website http://127.0.0.1:8000/index 或 http://127.0.0.1:8000/about！"
+
+    html = "Please check the website http://127.0.0.1:8000/index_html 或 http://127.0.0.1:8000/about_html"
     return HttpResponse(html)
 
-def index_views(request):
-    #Method 2
-    return HttpResponse("Rango says hey there partner!")
 
-def about_views(request):
-
-    return HttpResponse("Rango says here is the about page.")
-
-#Method 1
 def index_html(request):
-    from django.template import loader
-    return render(request, 'index.html')
-    #t = loader.get_template.rango('index.html')
-    #html = t.render()
-    #return HttpResponse(html)
 
-#Method 2
+    return render(request, 'index.html')
+
 def about_html(request):
-    from django.shortcuts import render
-   # return render(request, 'about.html')
+
+    return render(request, 'about.html')
--- tango_with_django_project_4/tango_with_django_project_4/settings.py
+++ tango_with_django_project_4/tango_with_django_project_4/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -43,7 +45,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'tango_with_django_project_4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/rango')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -118,9 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [STATIC_DIR, ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
